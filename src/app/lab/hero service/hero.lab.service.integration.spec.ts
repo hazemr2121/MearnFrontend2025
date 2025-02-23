6356fb8fb19c7afaeb6d0c61a7b004fe44d1d90d
hazemr2121 <ha.ahmed8996@gmail.com>
2025-02-23 20:38:25 +0200

    lab done

--- src/app/lab/hero service/hero.lab.service.integration.spec.ts
+++ src/app/lab/hero service/hero.lab.service.integration.spec.ts
@@ -1,5 +1,36 @@
-describe("3-hero service (http) integration testing:", () => {
-    
-    it("getHeroes function: send request and receive response successfully", () => { })
-    it("updateHero function: send request and receive response successfully", () => { })
-})
+import { of } from 'rxjs';
+import { HeroServiceForLab } from './hero.lab.service';
+import { Hero } from '../../hero';
+
+describe('3-hero service (http) integration testing:', () => {
+  let httpMock: any;
+  let heroService: HeroServiceForLab;
+  let heroesMock: Hero[];
+  let updatedHero: Hero;
+
+  beforeAll(() => {
+    httpMock = jasmine.createSpyObj(['get', 'put']);
+    heroService = new HeroServiceForLab(httpMock);
+    heroesMock = [
+      { id: 1, name: 'Hawkeye', strength: 100 },
+      { id: 2, name: 'Iroh', strength: 200 },
+      { id: 420, name: 'Snopp-dog', strength: 420 },
+    ];
+    httpMock.get.and.returnValue(of(heroesMock));
+
+    updatedHero = { id: 1, name: 'Hawkeye', strength: 150 };
+    httpMock.put.and.returnValue(of(updatedHero));
+  });
+
+  it('getHeroes function: send request and receive response successfully', () => {
+    heroService.getHeroes().subscribe((heroes) => {
+      expect(heroes).toEqual(heroesMock);
+    });
+  });
+
+  it('updateHero function: send request and receive response successfully', () => {
+    heroService.updateHero(updatedHero).subscribe((response) => {
+      expect(response).toEqual(updatedHero);
+    });
+  });
+});
--- src/app/lab/messages/messages.lab.component.spec.ts
+++ src/app/lab/messages/messages.lab.component.spec.ts
@@ -1,10 +1,50 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MessagesComponentForLab } from './messages.lab.component';
+import { MessageService } from '../../services/message/message.service';
+import { By } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
+describe('2-message component integration testing:', () => {
+  let component: MessagesComponentForLab;
+  let fixture: ComponentFixture<MessagesComponentForLab>;
+  let messageService: MessageService;
+  let element: HTMLElement;
 
-describe("2-message component integration testing:", () => {
-   
-   
-    it("expect component template to be empty", () => {
-        //Note: there is @if"messageService.messages.length" in line 1 in template
-    })
-    it("then expect div.msg to have the messages after setting it", () => {})
-})
+  beforeAll(() => {
+    TestBed.configureTestingModule({
+      imports: [MessagesComponentForLab],
+    });
+  });
+
+  afterAll(() => {
+    fixture.destroy();
+  });
+
+  it('expect component template to be empty', () => {
+    //Note: there is @if"messageService.messages.length" in line 1 in template
+    fixture = TestBed.createComponent(MessagesComponentForLab);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    element = fixture.debugElement.nativeElement;
+    // console.log(element.querySelector('h2')?.textContent);
+
+    expect(element.querySelector('h2')).toBeNull();
+  });
+
+  it('then expect div.msg to have the messages after setting it', () => {
+    messageService = TestBed.inject(MessageService);
+    messageService.add('first message');
+    messageService.add('second message');
+
+    fixture = TestBed.createComponent(MessagesComponentForLab);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    // component.ngOnChange();
+
+    const elements = fixture.debugElement.queryAll(By.css('div.msg'));
+    expect(elements.length).toEqual(2);
+    expect(elements[0].nativeElement.textContent).toContain('first message');
+    expect(elements[1].nativeElement.textContent).toContain('second message');
+  });
+});
